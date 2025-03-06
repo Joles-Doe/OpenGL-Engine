056c1976f3372a23fa63621891471cb3417ac6e9
Scott <scottlewwork@gmail.com>
2025-03-06 12:49:42 +0000

    Added Texture Class

---
+++ Texture.cpp
@@ -0,0 +1,88 @@
+#include "Texture.h"
+
+Texture::Texture(const std::string& _path) : mDirty(true), mID(0)
+{
+	Load(_path);
+}
+
+Texture::Texture(glm::ivec2 _size) : mDirty(true), mSize(_size), mID(0)
+{
+	mData.resize(_size.x * _size.y * 4);
+}
+
+Texture::~Texture()
+{
+}
+
+void Texture::SetSize(glm::ivec2 _size)
+{
+	mSize = _size;
+}
+
+glm::ivec2 Texture::GetSize() const
+{
+	return mSize;
+}
+
+void Texture::Load(const std::string& _path)
+{
+	unsigned char* textureData = stbi_load(_path.c_str(), &mSize.x, &mSize.y, NULL, 4);
+
+	if (!textureData)
+	{
+		throw std::runtime_error("Failed to Load Texture");
+	}
+
+	for (int x = 0; x < (mSize.x * mSize.y * 4); x++)
+	{
+		mData.push_back(textureData[x]);
+	}
+	free(textureData);
+}
+
+void Texture::SetPixel(glm::ivec2 _position, const glm::vec4& _color)
+{
+	int index = _position.x * _position.y * 4;
+	for (int x = 0; x < 4; x++)
+	{
+		mData[index + x] = _color[x];
+	}
+}
+
+glm::vec4 Texture::GetPixel(glm::ivec2 _position) const
+{
+	glm::vec4 pixel;
+	int index = _position.x * _position.y * 4;
+	for (int x = 0; x < 4; x++)
+	{
+		pixel[x] = mData[index + x];
+	}
+
+	return glm::vec4();
+}
+
+GLuint Texture::ID()
+{
+	if (mID == 0)
+	{
+		glGenTextures(1, &mID);
+
+		if (!mID)
+		{
+			throw std::runtime_error("Failed to Initialize Texture");
+		}
+	}
+	
+	if (mDirty == true)
+	{
+		glBindTexture(GL_TEXTURE_2D, mID);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mSize.x, mSize.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, mData.data());
+
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+		mDirty = false;
+	}
+}
---
+++ Texture.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <stb_image.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Texture
+{
+public:
+	Texture(const std::string& _path);
+	Texture(glm::ivec2 _size);
+	Texture(const Texture& _copy);
+	Texture& operator=(const Texture& _assign);
+	~Texture();
+
+	void SetSize(glm::ivec2 _size);
+	glm::ivec2 GetSize() const;
+
+	void Load(const std::string& _path);
+
+	void SetPixel(glm::ivec2 _position, const glm::vec4& _color);
+	glm::vec4 GetPixel(glm::ivec2 _position) const;
+
+	GLuint ID();
+
+private:
+	bool mDirty;
+	std::vector<float> mData;
+	glm::ivec2 mSize;
+	GLuint mID;
+};
+
